21ee437de8458526ffd089ff9c32e3c245d62bc7
Carmen <carmenmindeguia@hotmail.com>
2020-08-12 00:14:40 -0300

    prueba de publicacion

---
+++ build/precache-manifest.908ccf1ded2cfadcb0570aa191b6619d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4897ea365fa8ece7dd537b5d0db58b",
+    "url": "/Arlequin/index.html"
+  },
+  {
+    "revision": "8e4dd1ec42bf544f077c",
+    "url": "/Arlequin/static/css/main.f486d507.chunk.css"
+  },
+  {
+    "revision": "d5cc51d2b3461e14a6a5",
+    "url": "/Arlequin/static/js/2.e86e8292.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Arlequin/static/js/2.e86e8292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4dd1ec42bf544f077c",
+    "url": "/Arlequin/static/js/main.4c140aac.chunk.js"
+  },
+  {
+    "revision": "0a9a5a4ed31b1f33d21c",
+    "url": "/Arlequin/static/js/runtime-main.ba10614e.js"
+  },
+  {
+    "revision": "76dc50fe08e8e71a87cca82bd7fd8644",
+    "url": "/Arlequin/static/media/arlequin.76dc50fe.png"
+  },
+  {
+    "revision": "67064829618396ecde1b8d8a15f61922",
+    "url": "/Arlequin/static/media/bg-arlequin.67064829.jpg"
+  }
+]);
