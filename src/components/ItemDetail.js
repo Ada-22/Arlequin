e8b09d204423a4c990bb0004c48cd299141568ad
Carmen <carmenmindeguia@hotmail.com>
2020-09-02 03:37:19 -0300

    arreglando estilos y borrando funcion que no hace nada

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -9,7 +9,7 @@ function ItemDetail({ product }) {
       <li key={p.id}>
         <p className='col-md-12 p-0' key={p.id}><strong>{p.name}</strong><br/><span>{p.description}</span><br/>
         <span>Valor= $ {p.precio}</span></p>
-        <img className='img-fluid imgs mb-1'src={p.img} alt="product"/><br/></li>)}
+        <img className='img-fluid imgs_1 mb-1'src={p.img} alt="product"/><br/></li>)}
     </ul>
     </>
   }
--- src/components/ListItemsM.js
+++ src/components/ListItemsM.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import productStore from './Product-store';
+import {Link} from 'react-router-dom';
 
-function handleClick(e) {
-  e.preventDefault();
-  let mod = e.target.id;
-  return mod
-}
 
 function Mascaras({ products }) {
   let mascaras = products.filter(e => e.category === 'mascaras');
   return <>
   <h2 className='p-3 mt-5 text-center danger'>Nuestras Máscaras...</h2>
-  <ul className='row'>{mascaras.map((p) => 
-    <li onClick = {handleClick} id={p.id} className='col-md-4' key={p.id}>
-      <p>{p.name}</p>
-      <img className='img-fluid imgs mb-1'src={p.img} alt="product"/><br/>
-      <button className="btn btn-outline-danger mt-4 mb-5">Ver detalle</button></li>)}
-  </ul>
+  <div className='container'>
+    <ul className='row'>{mascaras.map((p) => 
+      <li id={p.id} className='col-md-4' key={p.id}>
+        <p>{p.name}</p>
+        <img className='img-fluid imgs mb-1'src={p.img} alt="product"/><br/>
+        <Link to='/detalle' className="btn btn-outline-danger mt-4 mb-5">Ver detalle</Link></li>)}
+    </ul>
+  </div>
   </>
 }
 function ItemsM() {
