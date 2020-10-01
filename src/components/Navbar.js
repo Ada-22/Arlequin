6720ff5b3a0b3f202044ff6ed34c0a899bd4311a
Carmen <carmenmindeguia@hotmail.com>
2020-09-30 22:41:17 -0300

    corrigiendo un estilo mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ()=>{
     return(
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
           <Logo/>
-          <h1 className='mr-5 mb-0' style={{fontSize: '16px'}}>Tienda de disfraces</h1>
+          <h1 className='mr-5 mb-0 title d-md-block d-sm-none' style={{fontSize: '16px'}}>Tienda de disfraces</h1>
           <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'/>
           </button>
