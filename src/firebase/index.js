bd6cfffef99961fd04ad1c99ccfc220272a9f654
Carmen <carmenmindeguia@hotmail.com>
2020-10-10 18:42:38 -0300

    actualiznado

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -8,8 +8,7 @@ const app = firebase.initializeApp ({
     projectId: "arlequin-df91c",
     storageBucket: "arlequin-df91c.appspot.com",
     messagingSenderId: "152823626782",
-    appId: "1:152823626782:web:6126bc24b715e001d5020f"
-});
+    appId: "1:152823626782:web:6126bc24b715e001d5020f"});
 
 export function getFirebase(){
     return app;
